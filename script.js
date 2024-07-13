b18a44f87868759f38517aa1ed9e2781998fded8
Pratyush Kumar <139534961+PratyushK9@users.noreply.github.com>
2024-07-13 12:34:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const hnKahoButton = document.getElementById('hn-kaho-btn');
+const noButton = document.getElementById('no-btn')
+const kabirAudio = document.getElementById('myAudio')
+
+
+
+hnKahoButton.addEventListener('click',()=>{
+    const tempPrompt = prompt('Pakka keh doon','Yes')
+    window.location.href="secondindex.html"
+})
+
+
